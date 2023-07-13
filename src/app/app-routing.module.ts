845e37bb8471c38b017877745426371eb1325aa3
Denis Stokin <stokindenis@gmail.com>
2023-07-13 13:26:44 +0300

    add routig

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {NewsComponent} from "./news/news.component";
+import {MyPageComponent} from "./my-page/my-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'MyPage', pathMatch: 'full' },
+  {path: 'news', component: NewsComponent},
+  {path: 'MyPage', component: MyPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {NewsComponent} from "./news/news.component";
 import {MyPageComponent} from "./my-page/my-page.component";
+import {HeaderComponent} from "./header/header.component";
+import {FooterComponent} from "./footer/footer.component";
 
 
 @NgModule({
   declarations: [
     AppComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NewsComponent,
     MyPageComponent,
+    FooterComponent,
+    HeaderComponent,
   ],
   providers: [],
   bootstrap: [AppComponent]
