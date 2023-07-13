629b39375804af9d1f4c65d8edc1aecef626b7eb
StokinDenis <stokindenis@gmail.com>
2023-07-13 20:14:50 +0300

    add input refactor name

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {FormsModule} from "@angular/forms";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {NewsComponent} from "./news/news.component";
@@ -21,6 +21,7 @@ import {FooterComponent} from "./footer/footer.component";
     MyPageComponent,
     FooterComponent,
     HeaderComponent,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/my-page/my-page.component.ts
+++ src/app/my-page/my-page.component.ts
@@ -1,16 +1,25 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, ElementRef} from '@angular/core';
+import {CommonModule, } from '@angular/common';
 import {ContentComponent} from "../content/content.component";
 import {FooterComponent} from "../footer/footer.component";
 import {HeaderComponent} from "../header/header.component";
+import {Renderer2} from "@angular/core";
 
 @Component({
   selector: 'app-my-page',
   standalone: true,
-  imports: [CommonModule, ContentComponent, FooterComponent, HeaderComponent],
+  imports: [CommonModule, ContentComponent, FooterComponent, HeaderComponent,],
   templateUrl: './my-page.component.html',
   styleUrls: ['./my-page.component.css']
 })
 export class MyPageComponent {
+
+
+  appearanceField(){
+    let field = this.elementRef.nativeElement.querySelector('.input_name_redactor')
+    this.renderer.setStyle(field, 'opacity','1')
+  }
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
+
 
 }
