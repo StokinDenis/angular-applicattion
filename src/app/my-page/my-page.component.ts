088d734335c5a002c0be5c6bb13c501a41611b16
StokinDenis <stokindenis@gmail.com>
2023-07-14 22:00:14 +0300

    add input refactor name

--- src/app/my-page/my-page.component.ts
+++ src/app/my-page/my-page.component.ts
@@ -4,11 +4,13 @@ import {ContentComponent} from "../content/content.component";
 import {FooterComponent} from "../footer/footer.component";
 import {HeaderComponent} from "../header/header.component";
 import {Renderer2} from "@angular/core";
+import {User} from "../user";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-my-page',
   standalone: true,
-  imports: [CommonModule, ContentComponent, FooterComponent, HeaderComponent,],
+  imports: [CommonModule, ContentComponent, FooterComponent, HeaderComponent, FormsModule,],
   templateUrl: './my-page.component.html',
   styleUrls: ['./my-page.component.css']
 })
@@ -17,9 +19,18 @@ export class MyPageComponent {
 
   appearanceField(){
     let field = this.elementRef.nativeElement.querySelector('.input_name_redactor')
+    let buttonChange = this.elementRef.nativeElement.querySelector('.button_change')
     this.renderer.setStyle(field, 'opacity','1')
+    this.renderer.setStyle(buttonChange, 'opacity', '1')
+  }
+
+  fieldHiding(){
+    let buttonChange = this.elementRef.nativeElement.querySelector('.button_change')
+    this.renderer.setStyle(buttonChange, 'opacity' , '0')
+    let field = this.elementRef.nativeElement.querySelector('.input_name_redactor')
+    this.renderer.setStyle(field, 'opacity' , '0')
   }
   constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
-
+  User:User={name:"Denis Stokin"}
 
 }
